4b09a3f1c1af1b5d86723223ed9aebf8e8a6aabb
Simon Gellis <simongellis@gmail.com>
2018-02-22 00:55:24 -0500

    Fix gitignore

--- index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function nameof<T>(): string;
--- transformer.d.ts
+++
@@ -1,2 +0,0 @@
-import * as ts from 'typescript';
-export default function nameofTransformer(ctx: ts.TransformationContext, program: ts.Program): ts.Transformer<ts.SourceFile>;
